6137123856f90f40565ce24b1a67110609534ec0
patrickquijano <patrickjosephquijano@gmail.com>
2019-12-07 15:57:05 +0800

    Included Url helper.

--- src/View/BootstrapViewTrait.php
+++ src/View/BootstrapViewTrait.php
@@ -5,15 +5,15 @@ namespace Bootstrap\View;
 trait BootstrapViewTrait {
 
     /**
-     * @param array $options
      * @return void
      */
-    public function initializeBootstrap(array $options = []) {
+    public function initializeBootstrap() {
         $this->loadHelper('Bootstrap.Bootstrap');
         $this->loadHelper('Html', ['className' => 'Bootstrap.BootstrapHtml']);
-        $this->loadHelper('Flash', ['className' => 'Bootstrap.BootstrapFlash']);
+        $this->loadHelper('Url', ['className' => 'Bootstrap.BootstrapHtml']);
         $this->loadHelper('Form', ['className' => 'Bootstrap.BootstrapForm']);
         $this->loadHelper('Paginator', ['className' => 'Bootstrap.BootstrapPaginator']);
+        $this->loadHelper('Flash', ['className' => 'Bootstrap.BootstrapFlash']);
     }
 
 }
