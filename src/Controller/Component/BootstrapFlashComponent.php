c21bd72541e61b8b9ec7373ac6bf5530e6dae007
patrickquijano <patrickjosephquijano@gmail.com>
2019-12-07 20:50:45 +0800

    Minor fixes

--- src/Controller/Component/BootstrapFlashComponent.php
+++ src/Controller/Component/BootstrapFlashComponent.php
@@ -22,10 +22,14 @@ class BootstrapFlashComponent extends FlashComponent {
      * @return void
      */
     public function __call($name, $args) {
+        $options = [
+            'plugin' => 'Bootstrap',
+            'escape' => false,
+        ];
         if (!empty($args[1])) {
-            $args[1] += ['plugin' => 'Bootstrap'];
+            $args[1] += $options;
         } else {
-            $args[1] = ['plugin' => 'Bootstrap'];
+            $args[1] = $options;
         }
         parent::__call($name, $args);
     }
