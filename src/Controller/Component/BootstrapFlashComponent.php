c622da7772bc867801641198f8819576ec0a909b
patrickquijano <patrickjosephquijano@gmail.com>
2020-01-04 18:06:47 +0800

    Added comments

--- src/Controller/Component/BootstrapFlashComponent.php
+++ src/Controller/Component/BootstrapFlashComponent.php
@@ -5,6 +5,8 @@ namespace Bootstrap\Controller\Component;
 use Cake\Controller\Component\FlashComponent;
 
 /**
+ * Bootstrap Flash component
+ * 
  * @method void primary(string $message, array $options = [])
  * @method void secondary(string $message, array $options = [])
  * @method void success(string $message, array $options = [])
@@ -17,9 +19,26 @@ use Cake\Controller\Component\FlashComponent;
 class BootstrapFlashComponent extends FlashComponent {
 
     /**
-     * @param string $name
-     * @param array $args
+     * Magic method for verbose flash methods based on element names.
+     *
+     * For example: $this->Flash->success('My message') would use the
+     * success.ctp element under `src/Template/Element/Flash` for rendering the
+     * flash message.
+     *
+     * If you make consecutive calls to this method, the messages will stack (if they are
+     * set with the same flash key)
+     *
+     * Note that the parameter `element` will be always overridden. In order to call a
+     * specific element from a plugin, you should set the `plugin` option in $args.
+     *
+     * For example: `$this->Flash->warning('My message', ['plugin' => 'PluginName'])` would
+     * use the warning.ctp element under `plugins/PluginName/src/Template/Element/Flash` for
+     * rendering the flash message.
+     *
+     * @param string $name Element name to use.
+     * @param array $args Parameters to pass when calling `FlashComponent::set()`.
      * @return void
+     * @throws \Cake\Http\Exception\InternalErrorException If missing the flash message.
      */
     public function __call($name, $args) {
         $options = [
